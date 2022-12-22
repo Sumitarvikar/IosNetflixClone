55802378e17a1768a2d809f3e38e6587ead76267
Sumitarviakr <sumitarvikar77@gmail.com>
2022-12-22 12:06:04 +0530

    files updated

--- Netflix-Clone/MainViewController.swift
+++ Netflix-Clone/MainViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
     }
 
 
